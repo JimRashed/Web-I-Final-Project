a06442ed27648de87ba979cfdddd13eb0b2333aa
JimmyRashed <[kildaradrago@gmail.com]>
2025-12-20 17:52:30 -0500

    Created static layout of result page

--- features/city-explorer/city-picker/result.js
+++ features/city-explorer/city-picker/result.js
@@ -1,1 +1,9 @@
 "use strict";
+
+//fetch stored city item
+const city = JSON.parse(localStorage.getItem("selectedCity"));
+
+const populationP = document.querySelector("#population");
+const coordinatesP = document.querySelector("#coordinates");
+const weatherP = document.querySelector("#weather");
+const flagImage = document.querySelector(".country img");
